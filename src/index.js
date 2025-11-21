3febbb53bd73860e8c7ab46d5db213ded4022435
key-68 <tkhoa06082005@gmailcom>
2025-11-21 23:30:15 +0700

    update index.js script

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ const app = require('./app');
 const PORT = process.env.PORT || 6868;
 
 // Kết nối database và chạy server
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-  });
-}).catch(error => {
-  console.error("Failed to start server:", error);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
+
