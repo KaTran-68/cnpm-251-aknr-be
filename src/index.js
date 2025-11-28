6b1563904d687bf05a9e6359ca73af3d22a5d126
key-68 <tkhoa06082005@gmailcom>
2025-11-28 17:05:56 +0700

    cors v3

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 require('dotenv').config(); // Đọc biến môi trường từ file .env
 const connectDB = require('./config/db'); // Import file kết nối database
 const app = require('./app');
-const cors = require('cors');
+const configCors = require("./config/cors");
 const PORT = process.env.PORT || 6868;
 
-app.use(cors({
-  origin: "http://localhost:3000",
-  methods: "GET,POST,PUT,DELETE",
-  credentials: true
-}));
+configCors(app)
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
